6b50c7e18d868437159bf82f395e1ebc6fd45792
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-03-31 23:26:28 +0100

    Split Characters
    split characters in string and added transitions to make it more interactable.

--- script.js
+++ script.js
@@ -3,28 +3,23 @@ const contTwo = document.querySelector(".two")
 const starryBackground = document.querySelectorAll(".test")
 const root = document.documentElement
 const navButtons = document.querySelector(".nav-list")
-const bounceLetters = ["My Name is Saif,", "A Front-end Web Developer"]
-const textArea = document.querySelector('.two-text')
+const introText = document.querySelector('.intro-text').children;
 
-// gsap.set(welcomeText,{autoAlpha:0})
-// gsap.to(welcomeText, { yPercent: -50, autoAlpha:1, ease: "power1.out"})
+for (let i = 0; i < introText.length; i++){
+    const text = introText[i];
+    const strText = text.textContent;
+    const splitText = strText.split('');
+    text.textContent = "";
+    let char = 0
 
-// gsap.to(starryBackground, {yPercent: -50, duration: 10 } )
-
-
-for(i = 0; i < bounceLetters.length; i++){
-    console.log(bounceLetters[i])
-    let lA = bounceLetters[i].split('')
-    console.log(lA)
-    for(i = 0 ; i < lA.length; i++){
-        x = lA[i]
-        if (x == ''){
-            console.log('working')
-            let spacing =  `&nbsp`
-            textArea.innerHTML += spacing
+    for (let i = 0; i < splitText.length; i++){
+        if (splitText[i] == " "){
+            text.innerHTML += "<span>" + splitText[i] + "</span>";
         }else{
-            let word = `<h1 class='bounce'> ${x} </h1>`
-            textArea.innerHTML += word
+            text.innerHTML += "<span class='bounce'>" + splitText[i] + "</span>";           
         }
     }
+
 }
+
+
