1d81ed40960b080d3f76495da513b224cef743d2
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-03-31 21:36:32 +0100

    Polishing Landing Page
    Added Chibi.
    Adding text animation.

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const contTwo = document.querySelector(".two")
 const starryBackground = document.querySelectorAll(".test")
 const root = document.documentElement
 const navButtons = document.querySelector(".nav-list")
+const bounceLetters = ["My Name is Saif,", "A Front-end Web Developer"]
+const textArea = document.querySelector('.two-text')
 
 // gsap.set(welcomeText,{autoAlpha:0})
 // gsap.to(welcomeText, { yPercent: -50, autoAlpha:1, ease: "power1.out"})
@@ -10,3 +12,19 @@ const navButtons = document.querySelector(".nav-list")
 // gsap.to(starryBackground, {yPercent: -50, duration: 10 } )
 
 
+for(i = 0; i < bounceLetters.length; i++){
+    console.log(bounceLetters[i])
+    let lA = bounceLetters[i].split('')
+    console.log(lA)
+    for(i = 0 ; i < lA.length; i++){
+        x = lA[i]
+        if (x == ''){
+            console.log('working')
+            let spacing =  `&nbsp`
+            textArea.innerHTML += spacing
+        }else{
+            let word = `<h1 class='bounce'> ${x} </h1>`
+            textArea.innerHTML += word
+        }
+    }
+}
