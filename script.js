4aae1c00cc2e081279b34eb71c5b60a69d1d8372
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-03-31 13:02:25 +0100

    Removed Gsap animations
    Deemed for the level of animation GSAP wasn't necessary.
    Added colour changes to buttons.
    Currently animating background.

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const starryBackground = document.querySelectorAll(".test")
 const root = document.documentElement
 const navButtons = document.querySelector(".nav-list")
 
-gsap.set(welcomeText,{autoAlpha:0})
-gsap.to(welcomeText, { yPercent: -50, autoAlpha:1, ease: "power1.out"})
+// gsap.set(welcomeText,{autoAlpha:0})
+// gsap.to(welcomeText, { yPercent: -50, autoAlpha:1, ease: "power1.out"})
 
-gsap.to(starryBackground, {yPercent: -50, duration: 10 } )
+// gsap.to(starryBackground, {yPercent: -50, duration: 10 } )
 
 
