4df0440e61b8747131ed8f9730c7a570e7289bac
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-04-16 23:51:32 +0100

    Dynamically added technologies used
    Used and if statement to determine if projects used specific technologies and dynamically added them to the projects.

--- app.js
+++ app.js
@@ -44,7 +44,21 @@ for (let i = 0; i < introText.length; i++){
 projectsContainer = document.querySelector("#project-container")
 
 function addProjects (data) {
+    
+
     for(let i = 0; i < data.length; i++){
+        function checkTrue(){
+            if(data[i].gsap == true){
+                return "<img src='./Other images/gsap.svg' width='40' alt='gsap'>"
+            }else if(data[i].React ==true){
+                return `<a title="Facebook, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:React-icon.svg"><img width="40" alt="React-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"></a>`
+            }
+            else{
+                return "<img src='./Other images/gsap.svg' width='40' alt='gsap'>"
+            }
+            
+        }
+
         let projectItem = 
         `<div class="project-item">
         <div class="project-item-repo">
@@ -61,7 +75,10 @@ function addProjects (data) {
                 <p>Technologies used:</p>
                 <a title="W3C, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:HTML5_logo_and_wordmark.svg"><img width="40" alt="HTML5 logo and wordmark" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"></a>
                 <a title="Rudloff, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:CSS3_logo_and_wordmark.svg"><img width="30" alt="CSS3 logo and wordmark" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/256px-CSS3_logo_and_wordmark.svg.png"></a>
-                <a title="Chris Williams, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Unofficial_JavaScript_logo_2.svg"><img width="40" alt="Unofficial JavaScript logo 2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png"></a>            </div>
+                <a title="Chris Williams, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Unofficial_JavaScript_logo_2.svg"><img width="40" alt="Unofficial JavaScript logo 2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png"></a>
+                ${checkTrue()}
+            
+            </div>
         </div>`
         projectsContainer.innerHTML+= projectItem
     }
