ac0da8a8758033145944dd3b68acf0e3f4fdfe80
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-05-02 01:41:24 +0100

    Update to style
    Changed style and added new icons

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ function addProjects (data) {
     for(let i = 0; i < data.length; i++){
         function checkTrue(){
             if(data[i].gsap == true){
-                return "<img src='./Other images/gsap.svg' width='40' alt='gsap'>"
+                return `<img src="./Icons/gsap.svg" alt="Gsap">`
             }else if(data[i].React ==true){
-                return `<a title="Facebook, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:React-icon.svg"><img width="40" alt="React-icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"></a>`
+                return `<img src="./Icons/react.svg" alt="React">`
             }
             else{
-                return "<img src='./Other images/gsap.svg' width='40' alt='gsap'>"
+                return ``
             }
             
         }
@@ -64,8 +64,8 @@ function addProjects (data) {
         <div class="project-item-repo">
             <img class="item-image" src="${data[i].url}" alt="project image"/>
             <div class="item-repo">
-                <div>LIVE</div>
-                <div>GITHUB</div>
+                <img class="item-repo-icons" src="./Icons/website-hosting.svg" alt="Live">
+                <img class="item-repo-icons" src="./Icons/github-icon.svg" alt="Github"/> 
             </div> 
         </div>
         <div class="project-item-text">
@@ -73,9 +73,9 @@ function addProjects (data) {
             <p>${data[i].description}</p>
             <div class="project-item-tech">
                 <p>Technologies used:</p>
-                <a title="W3C, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:HTML5_logo_and_wordmark.svg"><img width="40" alt="HTML5 logo and wordmark" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/512px-HTML5_logo_and_wordmark.svg.png"></a>
-                <a title="Rudloff, CC BY 3.0 &lt;https://creativecommons.org/licenses/by/3.0&gt;, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:CSS3_logo_and_wordmark.svg"><img width="30" alt="CSS3 logo and wordmark" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/256px-CSS3_logo_and_wordmark.svg.png"></a>
-                <a title="Chris Williams, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Unofficial_JavaScript_logo_2.svg"><img width="40" alt="Unofficial JavaScript logo 2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png"></a>
+                <img src="./Icons/html.svg" alt="HTML">
+                <img src="./Icons/css.svg" alt="CSS">
+                <img src="./Icons/javascript.svg" alt="Javascript">
                 ${checkTrue()}
             
             </div>
