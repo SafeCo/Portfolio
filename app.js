38c141a4e8b097fa52836a5ac3bbbee643ab543e
SafeCo <77068178+SafeCo@users.noreply.github.com>
2022-05-07 11:21:12 +0100

    Updates to style.css
    changes made to make more responsive

--- app.js
+++ app.js
@@ -63,25 +63,26 @@ function addProjects (data) {
 
         let projectItem = 
         `<div class="project-item">
-        <div class="project-item-repo">
-            <img class="item-image" src="${data[i].image}" alt="project image"/>
-            <div class="item-repo">
-                <a href="${"https://"+domainName+data[i].url}"> <img class="item-repo-icons" src="./Icons/website-hosting.svg" alt="Live"> </a>
-                <a href="${"https://github.com/SafeCo"+data[i].url}"> <img class="item-repo-icons" src="./Icons/github-icon.svg" alt="Github"/> </a>
-            </div> 
-        </div>
-        <div class="project-item-text">
-            <h3>${data[i].title}</h3>
-            <p>${data[i].description}</p>
+            <div class="project-item-repo">
+                <img class="item-image" src="${data[i].image}" alt="project image"/>
+                <div class="item-repo">
+                    <a href="${"https://"+domainName+data[i].url}"> <img class="item-repo-icons" src="./Icons/website-hosting.svg" alt="Live"> </a>
+                    <a href="${"https://github.com/SafeCo"+data[i].url}"> <img class="item-repo-icons" src="./Icons/github-icon.svg" alt="Github"/> </a>
+                </div> 
+            </div>
+            <div class="project-item-text">
+                <h3>${data[i].title}</h3>
+                <p>${data[i].description}</p>
+            </div>
             <div class="project-item-tech">
-                <p>Technologies used:</p>
-                <img src="./Icons/html.svg" alt="HTML">
-                <img src="./Icons/css.svg" alt="CSS">
-                <img src="./Icons/javascript.svg" alt="Javascript">
-                ${checkTrue()}
-            
+                    <p>Technologies used:</p>
+                    <img src="./Icons/html.svg" alt="HTML">
+                    <img src="./Icons/css.svg" alt="CSS">
+                    <img src="./Icons/javascript.svg" alt="Javascript">
+                    ${checkTrue()}
             </div>
         </div>`
+        
         projectsContainer.innerHTML+= projectItem
     }
     projectListeners()
